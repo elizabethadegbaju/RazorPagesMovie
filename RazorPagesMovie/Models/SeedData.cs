22af9dfbebe8d7caddd8b61b48830af17a748909
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-02-19 21:21:33 +0100

    feat: Add movie rating

--- RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/Models/SeedData.cs
@@ -22,6 +22,7 @@ namespace RazorPagesMovie.Models
                     new Movie
                     {
                         Title = "When Harry Met Sally",
+                        Rating = "R",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
                         Price = 7.99M
@@ -30,6 +31,7 @@ namespace RazorPagesMovie.Models
                     new Movie
                     {
                         Title = "Ghostbusters ",
+                        Rating = "PG-13",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
                         Price = 8.99M
@@ -38,6 +40,7 @@ namespace RazorPagesMovie.Models
                     new Movie
                     {
                         Title = "Ghostbusters 2",
+                        Rating = "PG-13",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
                         Price = 9.99M
@@ -46,6 +49,7 @@ namespace RazorPagesMovie.Models
                     new Movie
                     {
                         Title = "Rio Bravo",
+                        Rating = "PG-13",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
                         Price = 3.99M
