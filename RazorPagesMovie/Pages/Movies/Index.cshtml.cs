4b7ef882c1fd7de04d2d7f5947031322f243b6ea
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-02-19 19:42:01 +0100

    feat: Add filter movies by title

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -19,11 +20,23 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = from m in _context.Movie select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            Movie = await movies.ToListAsync();
         }
     }
 }
